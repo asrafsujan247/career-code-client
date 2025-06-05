c061b3cc25a7828669f3ceee30eea49747580d2b
asrafsujan247 <asraf.sujan247@gmail.com>
2025-06-05 13:04:51 +0600

    made the basic structure and installed react router and tailwind, daisyui

---
+++ src/router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-3xl text-white">Hello World</div>,
+  },
+]);
+
+export default router;
